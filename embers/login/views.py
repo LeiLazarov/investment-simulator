d0540845f47ecf2ebcc8e5af54b6e2cd3b549c16
leilazarov <stoneiiilei@gmail.com>
2020-10-08 17:48:24 +1100

    building basic development structure

---
+++ embers/login/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login/login.html')
+
+
+def registration(request):
+    return render(request, 'login/registration.html')
+
+
