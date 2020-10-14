9b3a7b62e971ca5ed25044d6b9db6ee812d5873c
FFFFFFox <56993707+FFFFFFox@users.noreply.github.com>
2020-10-14 21:53:42 +0800

    1. Add a tutorial page 2. update the logo image 3. modify elements of navigation bar 4. update my work diary

--- embers/embers/settings.py
+++ embers/embers/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login',
     'contact_us',   # added by Yiren
+    'tutorial'
 ]
 
 MIDDLEWARE = [
--- embers/tutorial/urls.py
+++ embers/tutorial/urls.py
@@ -13,15 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
 
+from . import views
+
+app_name = 'tutorial'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('', include('login.urls')),
-    path('', include('contact_us.urls')),
-    path('', include('tutorial.urls')),
+    path('tutorial/', views.intro, name='intro'),
 ]
