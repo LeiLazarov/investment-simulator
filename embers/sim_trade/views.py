e59243371ec34061f80511892e81b764058a4aad
YirenHu <huyiren05@gmail.com>
2020-10-29 11:39:00 +0800

    sim_trade/stock.html & views.py

--- embers/sim_trade/views.py
+++ embers/sim_trade/views.py
@@ -8,7 +8,39 @@ import json
 import datetime
 # Create your views here.
 
+# Yiren 的 sim_trade/views在下面
 
+def sim_trade(request):
+    if request.method == 'GET':
+        return render(request, 'sim_trade/sim_trade.html')
+
+    elif request.method == 'POST':
+        stock_code = request.POST.get('stock_code').upper()
+        stock_quote = requests.get(
+            'https://finnhub.io/api/v1/quote?symbol=' + stock_code + '&token=buajtbf48v6ocn3pc8ug')
+        company_info = requests.get(
+            'https://finnhub.io/api/v1/stock/profile2?symbol=' + stock_code + '&token=buajtbf48v6ocn3pc8ug'
+        )
+
+        # stock_candles = requests.get('https://finnhub.io/api/v1/stock/candle?symbol=' + stock_code
+        #                              + '&resolution=D&from=1572651390&to=1572910590&token=buajtbf48v6ocn3pc8ug')
+        # print(stock_quote.json())
+        # print(company_info.json())
+
+        if len(company_info.json()) == 0:
+            found_stock_flag = 0
+            return render(request, 'sim_trade/not_found_stock.html')
+        else:
+            found_stock_flag = 1
+
+        return render(request, 'sim_trade/stock.html', {'stock': stock_quote.json(), 'company_info': company_info.json(),
+                                                        'found_stock_flag': found_stock_flag})
+
+def follow_post(request):
+
+    return render(request)
+
+'''
 def sim_trade(request):
     if request.method == 'GET':
         # print('get')
@@ -34,7 +66,7 @@ def sim_trade(request):
         return render(request, 'sim_trade/stock.html', {'stock': stock_quote.json(), 'company_info': company_info.json(),
                                                         'stock_time': stock_time})
 
-
+'''
 def detail(request):
     stock = models.Detail.objects.get(pk=1)
     # json_data = stock.candle
