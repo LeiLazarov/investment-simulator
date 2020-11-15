5af90e5b6c16aef80ffc9105799fb0ad1ff0d549
Yue <z5238570@ad.unsw.edu.au>
2020-11-15 20:19:35 +0800

    Add comments

--- embers/watchlist/views.py
+++ embers/watchlist/views.py
@@ -16,7 +16,7 @@ def watchlist(request):
         return redirect("/login/")
     watchlist_account = WatchList.objects.filter(user_id=uid).values()
     watchlist_list = []
-
+# Retrieve stock information from "stock.model"
     for e in watchlist_account:
         WL = {}
         WL['id'] = e['id']
@@ -24,6 +24,7 @@ def watchlist(request):
         WL['c'] = Stock.objects.get(symbol=e['symbol']).price
         WL['pc'] = Stock.objects.get(symbol=e['symbol']).close
         WL['chg'] = WL['c'] - WL['pc']
+# Percentage Change = (current unit price- previous day's unit price) / previous day's unit price
         WL['res'] = "{:.3f}".format( WL['chg'] * 100 / WL['pc'])
         WL['upd'] = Stock.objects.get(symbol=e['symbol']).updateAt
         WL['createdAt'] = e['createdAt']
@@ -31,7 +32,7 @@ def watchlist(request):
     
     return render(request, 'watchlist/watchlist.html', {'my_watchlist': watchlist_list})
 
-
+# remove function of remove bottom
 def delete(request):
     uid = request.session.get('user_id', '')
     delete_id = request.GET.get('delete_id')
@@ -40,24 +41,4 @@ def delete(request):
     return redirect('/watchlist/')
 
 
-# def reviews(request):
-#     review_list = Review.objects.all()
-#     page_number = request.GET.get('page', 1)
-#     paginator = Paginator(review_list, 2)
-#     try:
-#         page_reviews = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         page_reviews = paginator.page(1)
-#     except EmptyPage:
-#         page_reviews = paginator.page(paginator.num_pages)
-#
-#     return render(request, 'reviews.html', {'page_reviews': page_reviews})
-#
-# def watchlist_detail(request, id):
-#      stock_detail = WatchList.objects.get(pk=id)
-#      stock_quote = requests.get('https://finnhub.io/api/v1/quote?symbol=' + stock_detail.symbol + '&token=buajtbf48v6ocn3pc8ug')
-#      company_info = requests.get('https://finnhub.io/api/v1/stock/profile2?symbol=' + stock_detail.symbol + '&token=buajtbf48v6ocn3pc8ug')
-#      stock_detail.stock_quote = stock_quote.json()
-#      stock_detail.company_info = company_info.json()
-#      return render(request, 'watchlist/watchlist_detail.html', {'stock_detail': stock_detail})
 
