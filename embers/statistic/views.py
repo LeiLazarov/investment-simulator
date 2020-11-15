e624c58ad363c86550d544341f07144f0881abec
Yue <z5238570@ad.unsw.edu.au>
2020-11-15 22:25:04 +0800

    add comments

--- embers/statistic/views.py
+++ embers/statistic/views.py
@@ -15,13 +15,16 @@ def table(request):
 
 
 def account_record(request):
+    #check the user_id
     uid = request.session.get('user_id', '')
     if not uid:
         return redirect("/login/")
-    #uid = 1
+    
+    #get the record according to uid
     record_account = sim_models.Record.objects.filter(user_id=uid).values()
     record_list = []
     
+    #change the record data to fit the table 
     for i in range(len(record_account)):
         record = {}
         record['id'] = i + 1
@@ -36,33 +39,41 @@ def account_record(request):
     return render(request, 'record/record.html',{'record': record_list})
 
 def analysis(request):
+    #check uid
     uid = request.session.get('user_id', '')
     if not uid:
         return redirect("/login/")
+    
+    #get the owned data 
     record_account = sim_models.Owned.objects.filter(user_id=uid).values()
+
+    #mark the total paid value 
     total_value = 0
     record_value = []
-    for e in record_account:
-       
+
+    #change the data to json form
+    for e in record_account:     
         value = {}
-
-        value['stockname'] = stock_models.Stock.objects.get(id= e['stock_id']).symbol
-        
+        value['stockname'] = stock_models.Stock.objects.get(id= e['stock_id']).symbol      
         value['unit'] = e['quantity']
-        value['owned_value'] = e['avg_price'] * e['quantity']
-        value['current_value'] = stock_models.Stock.objects.get(id= e['stock_id']).price * e['quantity']
-        e = value['current_value'] - value['owned_value']
-        if e > 0:
-            value['profit'] = float(e)
-            value['loss'] = 0
-        else:
-            value['profit'] = 0
-            value['loss'] = float(e)
-
+        value['owned_value'] = e['avg_price'] * e['quantity'] # total paid value
+        value['current_value'] = stock_models.Stock.objects.get(id= e['stock_id']).price * e['quantity'] #current value
+       
         value['owned_value'] = float(value['owned_value'])
         value['current_value'] = float(value['current_value'])
+
+        e = value['current_value'] - value['owned_value']
+        if e > 0:
+            value['profit'] = e
+        else:
+            value['loss'] = e
+
+        
+        #calculate the total paid value
         total_value +=  value['owned_value']
         record_value.append(value)
+
+    #set the threshold of data
     value_thr = total_value * 0.01
 
     return render(request, 'record/analysis.html', {'account': record_value, 'value_thr': value_thr})
